1e31304344a112a8f53896849da6c2a8bd272f66
Jun <junsoopark92@gmail.com>
2022-08-24 15:05:27 -0400

    add nltk

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import random
 import sys
 from termcolor import colored
 import nltk
+nltk.download('words')
 from nltk.corpus import words
 
 def print_menu():
     print("Let's play Wordle:")
     print("Type a 5 letter word and hit enter!")
 
-
 def read_random_word():
     with open("words.txt") as f:
         words = f.read().splitlines()
         return random.choice(words)
 
+nltk.data.path.append('/work/words')
+word_list = words.words()
+words_five = [word for word in word_list if len(word) == 5]
+
 print_menu()
 play_again = ''
-
 while play_again != "q":
-    word = read_random_word()
+    # word = read_random_word()
+    word = random.choice(words_five)
     for attempt in range(1, 7):
         guess = input().lower()
 
@@ -37,4 +41,6 @@ while play_again != "q":
         if guess == word:
             print(colored(f"Congrats! You got the wordle in {i}", 'red'))
             break
+        elif attempt == 6:
+            print(f'Sorry the wordle was .. {word}')
     play_again = input("Want to play agian? Type q to exit ")
